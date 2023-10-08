9ba81b72dc6b94c3dde8dd75469b051ec2c43d60
raihaannr <107612636+raihaannr@users.noreply.github.com>
2023-10-08 18:15:18 +0800

    Add files via upload

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color correct = Color.fromRGBO(76, 175, 80, 1);
+const Color incorrect = Color.fromRGBO(244, 67, 54, 1);
+const Color neutral = Color.fromRGBO(240, 240, 240, 1);
+const Color background = Color.fromARGB(255, 0, 78, 142);
